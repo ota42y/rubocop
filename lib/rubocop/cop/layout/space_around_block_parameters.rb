52e859289e93f862dfa13fde909a47b128db92c7
ota42y <ota42y@gmail.com>
2019-06-28 11:54:18 +0900

    Revert "Fix infinite loop for `Layout/SpaceAroundBlockParameters` with `EnforcedStyleInsidePipes: :space` (#6798)"
    This reverts commit cf18052dbe57acd1b42d69c5333f180d1dcaffd1.

--- lib/rubocop/cop/layout/space_around_block_parameters.rb
+++ lib/rubocop/cop/layout/space_around_block_parameters.rb
@@ -36,19 +36,11 @@ module RuboCop
           check_each_arg(arguments)
         end
 
-        # @param target [RuboCop::AST::Node,Parser::Source::Range]
-        def autocorrect(target)
+        def autocorrect(range)
           lambda do |corrector|
-            if target.is_a?(RuboCop::AST::Node)
-              if target.parent.children.first == target
-                corrector.insert_before(target.source_range, ' ')
-              else
-                corrector.insert_after(target.source_range, ' ')
-              end
-            elsif target.source =~ /^\s+$/
-              corrector.remove(target)
-            else
-              corrector.insert_after(target, ' ')
+            case range.source
+            when /^\s+$/ then corrector.remove(range)
+            else              corrector.insert_after(range, ' ')
             end
           end
         end
@@ -105,12 +97,11 @@ module RuboCop
         end
 
         def check_opening_pipe_space(args, opening_pipe)
-          first_arg = args.first
-          range = first_arg.source_range
+          first = args.first.source_range
 
-          check_space(opening_pipe.end_pos, range.begin_pos, range,
-                      'before first block parameter', first_arg)
-          check_no_space(opening_pipe.end_pos, range.begin_pos - 1,
+          check_space(opening_pipe.end_pos, first.begin_pos, first,
+                      'before first block parameter')
+          check_no_space(opening_pipe.end_pos, first.begin_pos - 1,
                          'Extra space before first')
         end
 
@@ -145,11 +136,10 @@ module RuboCop
           )
         end
 
-        def check_space(space_begin_pos, space_end_pos, range, msg, node = nil)
+        def check_space(space_begin_pos, space_end_pos, range, msg)
           return if space_begin_pos != space_end_pos
 
-          target = node || range
-          add_offense(target, location: range, message: "Space #{msg} missing.")
+          add_offense(range, location: range, message: "Space #{msg} missing.")
         end
 
         def check_no_space(space_begin_pos, space_end_pos, msg)
